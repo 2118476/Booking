36902ec8df3133c41b4b15de2e6c51ce1fefa37f
2118476 <117785648+2118476@users.noreply.github.com>
2024-08-23 16:55:54 +0100

    Initial commit for backend

---
+++ src/main/java/com/example/hair_salon_backend/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.hair_salon_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000","https://hair-salon-booking-da2658e241f8.herokuapp.com/") // Add your
+                                                                                                         // Netlify
+                                                                                                         // domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
